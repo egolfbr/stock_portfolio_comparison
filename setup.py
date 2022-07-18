ef69d4265c853f8fd9e8bc97b7f895d48ae36bb2
Brian Egolf <47579932+egolfbr@users.noreply.github.com>
2022-07-17 21:15:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='fund preformance comparison',
-      version='1.0',
+setup(name='fund performance comparison',
+      version='1.5',
       description='Compare fund preformance to S&P 500 ETF',
       author='Brian Egolf',
       author_email='egolfbr@miamioh.edu',
       url='github.com',
-      py_modules=["fund_preformance"],
+      py_modules=["fund_performance"],
      )
